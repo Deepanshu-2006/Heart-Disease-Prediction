620a45ca76eb73ce23555f19353362402e314ff0
Deepanshu Khatri <deepanshukhatri20061972@gmail.com>
2026-05-07 18:46:50 +0530

    Fix try/except syntax for pkl loading

--- app.py
+++ app.py
@@ -107,7 +107,6 @@ try:
     cat_imp = joblib.load(os.path.join(BASE_DIR, 'cat_imputer.pkl'))
     scr     = joblib.load(os.path.join(BASE_DIR, 'scaler.pkl'))
     enc     = joblib.load(os.path.join(BASE_DIR, 'encoder.pkl'))
-
 except Exception as e:
     print("Warning: Could not load the machine learning model. Make sure to run the pipeline first.", e)
 
